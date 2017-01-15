9b54f3b270309548df366f4ce386aed83e12262d
Alexander Akov <quidjiboo20015@gmail.com>
2017-01-15 19:42:06 +0400

    123

--- app/src/main/java/ru/akov/hairn/Test_chooser.java
+++ app/src/main/java/ru/akov/hairn/Test_chooser.java
@@ -6,9 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Test_chooser extends AppCompatActivity {
-
+    CalendarView calendarView;
+    TextView dateDisplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +28,21 @@ public class Test_chooser extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        calendarView = (CalendarView) findViewById(R.id.calendarView2);
+
+        dateDisplay = (TextView) findViewById(R.id.date_display);
+        dateDisplay.setText("Date: ");
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+                dateDisplay.setText("Date: " + i2 + " / " + i1 +1 + " / " + i);
+
+                Toast.makeText(getApplicationContext(), "Selected Date:\n" + "Day = " + i2 + "\n" + "Month = " + i1 + 1 + "\n" + "Year = " + i, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
     }
 
-}
+
+
